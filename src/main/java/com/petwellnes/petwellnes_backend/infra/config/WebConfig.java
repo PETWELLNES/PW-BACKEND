78d285711a2e474d542e68c3a017019d012b1bf5
Adrian Cisneros <acisnerosb1@upao.edu.pe>
2024-07-03 20:50:46 -0500

    webconfig corregido??

--- src/main/java/com/petwellnes/petwellnes_backend/infra/config/WebConfig.java
+++ src/main/java/com/petwellnes/petwellnes_backend/infra/config/WebConfig.java
@@ -1,29 +1,45 @@
 package com.petwellnes.petwellnes_backend.infra.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Value("${upload.path}")
-    private String uploadPath;
+import java.io.IOException;
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class WebConfig implements Filter {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // TODO Auto-generated method stub
     }
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + uploadPath + "/");
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, HEAD, PATCH");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, credential, X-XSRF-TOKEN");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
     }
 }
